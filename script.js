cf787d1c370de99a423ad87ce40bc0b826e41d7f
carlosuwono <carlosuwono37@gmail.com>
2026-05-29 05:37:55 +1000

    played around with the layout and added a divider

--- script.js
+++ script.js
@@ -2,44 +2,55 @@ window.addEventListener("load", (event) => {
   console.log("page is fully loaded");
 
 
-document.getElementById("trigger").addEventListener("mouseenter", blackEffect);
-document.getElementById("trigger2").addEventListener("mouseenter", yellowEffect);
+document.getElementById("SITP1").addEventListener("mouseenter", SITP1);
+document.getElementById("SITP2").addEventListener("mouseenter", SITP2);
+document.getElementById("SITP3").addEventListener("mouseenter", SITP3);
+document.getElementById("SITPQ").addEventListener("mouseenter", SITPQ);
+document.getElementById("TBFAPQ").addEventListener("mouseenter", TBFAPQ);
+document.getElementById("GOQ").addEventListener("mouseenter", GOQ);
+document.getElementById("BPQ").addEventListener("mouseenter", BPQ);
+document.getElementById("TCTQ").addEventListener("mouseenter", TCTQ);
 
-function blackEffect() {
-  document.getElementById("cover").classList.remove("yellow");
-  document.getElementById("cover").classList.add("active");
-  console.log("mouse enter ")
-  
+function SITP1() {
+  document.getElementById("SITP1").classList.add("after");
+  console.log("mouse enter")
 }
 
-function yellowEffect() {
-  document.getElementById("cover").classList.remove("active");
-  document.getElementById("cover").classList.add("yellow");
-  console.log("mouse enter ")
-  
+function SITP2() {
+  document.getElementById("SITP2").classList.add("after");
+  console.log("mouse enter")
 }
 
+function SITP3() {
+  document.getElementById("SITP3").classList.add("after");
+  console.log("mouse enter")
+}
+
+function SITPQ() {
+  document.getElementById("SITPQ").classList.add("after");
+  console.log("mouse enter")
+}
+
+function TBFAPQ() {
+  document.getElementById("TBFAPQ").classList.add("after");
+  console.log("mouse enter")
+}
+
+function GOQ() {
+  document.getElementById("GOQ").classList.add("after");
+  console.log("mouse enter")
+}
+
+function BPQ() {
+  document.getElementById("BPQ").classList.add("after");
+  console.log("mouse enter")
+}
+
+function TCTQ() {
+  document.getElementById("TCTQ").classList.add("after");
+  console.log("mouse enter")
+}
 
 console.log("hello world")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 });
 
