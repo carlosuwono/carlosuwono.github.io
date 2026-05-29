58a440ac116bd662ab0451eae477e62bbd0b5495
carlosuwono <carlosuwono37@gmail.com>
2026-05-29 16:10:28 +1000

    added a nav bar, fixed some layout, yadayada

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 window.addEventListener("load", (event) => {
   console.log("page is fully loaded");
 
+let hour = 00;
+let minute = 00;
+let second = 00;
+let count = 00;
+
+window.onload = stopWatch()
 
 document.getElementById("SITP1").addEventListener("mouseenter", SITP1);
 document.getElementById("SITP2").addEventListener("mouseenter", SITP2);
@@ -10,6 +16,54 @@ document.getElementById("TBFAPQ").addEventListener("mouseenter", TBFAPQ);
 document.getElementById("GOQ").addEventListener("mouseenter", GOQ);
 document.getElementById("BPQ").addEventListener("mouseenter", BPQ);
 document.getElementById("TCTQ").addEventListener("mouseenter", TCTQ);
+
+function stopWatch() {
+        count++;
+
+        if (count == 100) {
+            second++;
+            count = 0;
+        }
+
+        if (second == 60) {
+            minute++;
+            second = 0;
+        }
+
+        if (minute == 60) {
+            hour++;
+            minute = 0;
+            second = 0;
+        }
+
+        let hrString = hour;
+        let minString = minute;
+        let secString = second;
+        let countString = count;
+
+        if (hour < 10) {
+            hrString = "0" + hrString;
+        }
+
+        if (minute < 10) {
+            minString = "0" + minString;
+        }
+
+        if (second < 10) {
+            secString = "0" + secString;
+        }
+
+        if (count < 10) {
+            countString = "0" + countString;
+        }
+
+        document.getElementById("hr").innerHTML = hrString;
+        document.getElementById("min").innerHTML = minString;
+        document.getElementById("sec").innerHTML = secString;
+        document.getElementById("count").innerHTML = countString;
+        setTimeout(stopWatch, 10);
+    }
+
 
 function SITP1() {
   document.getElementById("SITP1").classList.add("after");
